0b78aad75f8a64e1fdba25465446ed268cb25b16
Jeevan kumar <jeevankumarkorra2005@gmail.com>
2024-10-25 08:56:21 +0530

    prisma chacing erorr in deployment

--- backend/index.js
+++ backend/index.js
@@ -8,3 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/emails', emailRoutes);
 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+})
